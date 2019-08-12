d93cab5b2178dc7f0038c1ba77d214d63ef63330
shotaro427 <toshinobu.s111@gmail.com>
2019-08-12 12:48:41 +0900

    first commit

---
+++ WorldHeritageQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldHeritageQuiz
+//
+//  Created by 田内　翔太郎 on 2019/08/12.
+//  Copyright © 2019 田内　翔太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
