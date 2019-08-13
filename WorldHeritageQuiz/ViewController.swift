bd207194932c52fc66a83d3eea2cc4082e22ffc8
shotaro427 <toshinobu.s111@gmail.com>
2019-08-13 19:39:09 +0900

    ストーリーボードの編集

--- WorldHeritageQuiz/ViewController.swift
+++ WorldHeritageQuiz/ViewController.swift
@@ -12,19 +12,25 @@ class ViewController: UIViewController {
     
     // 何問目かを表示する
     @IBOutlet weak var titleOfQuestion: UINavigationItem!
-    // 問1~3までのview
-    @IBOutlet var questions: [UIView]!
+    // 問題を表示するtextView
+    @IBOutlet weak var questionTextView: UITextView!
     // スタックビュー（回答ボタン）
     @IBOutlet weak var answersStackView: UIStackView!
+    
+    // 問題文
+    let questions: [String] = [
+        "問１日本の世界遺産『富士山－信仰の対象と芸術の源泉』は、2013年に（ ）として世界遺産登録されました。\n1. 文化遺産\n2. 自然遺産\n3. 山岳遺産\n4. 伝統遺産",
+        "問2イタリア共和国の世界遺産『フィレンツェの歴史地区』のあるフィレンツェを中心に、17世紀に栄えた芸術運動は何でしょうか。\n1. シュルレアリスム\n2. アバンギャルド\n3. ルネサンス",
+        "問3 2016年のオリンピック開催地であるリオ・デ・ジャネイロで、ブラジル独立100周年を記念して作られたキリスト像が立つ場所として、正しいものはどれか。\n1. コパカバーナの山\n2. コルコバードの丘"
+    ]
+    // 問題の答え
+    let answers: [Int] = [1, 3, 2]
     
     // 結果を表示する画面に渡す情報群
     // 総問題数
     var questionsCount: Int = 0
     // 正解、不正解を判断する配列(true = 正解、false = 不正解)
     var correctOrIncorrect: [Bool] = []
-    
-    // 問題の答え
-    let answers: [Int] = [1, 3, 2]
     // 現在の問題番号を格納する変数
     var questionNumber: Int = 1
     
@@ -32,8 +38,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // 総問題数を格納する
         questionsCount = questions.count
-        // 「1問目」と表示させる
-        titleOfQuestion.title = "\(questionNumber)問目"
     }
     
     // 問題から結果の画面へ遷移したタイミングの処理
@@ -51,8 +55,6 @@ class ViewController: UIViewController {
     // =====================================================
     // 回答を確認する関数
     func checkAnswer(playerAnswer: Int) {
-        
-        
         // 回答があっているか確認
         if playerAnswer == answers[questionNumber - 1] {
             if questionNumber != questions.count {
@@ -67,12 +69,10 @@ class ViewController: UIViewController {
                 // アラートを表示
                 showAlertWhenCorrect(title: "正解です！", message: "結果を表示します。")
             }
-                
         } else {
             // アラートを表示
             showAlertWhenIncorrect(title: "不正解です...", message: "もう一度挑戦しますか？")
         }
-        
     }
     
     // =====================================================
@@ -131,12 +131,12 @@ class ViewController: UIViewController {
     func nextQuestion() {
         // questionNumberが問題数を超えないようにする
         if questionNumber < questions.count  {
-            // viewを隠す
-            questions[questionNumber - 1].isHidden = true
             // ボタンを減らす
             hideButton()
             // 問題を次に進める
             questionNumber += 1
+            // 次の問題を表示する
+            questionTextView.text = questions[questionNumber - 1]
             // タイトルを変更する
             titleOfQuestion.title = "\(questionNumber)問目"
         } else {
@@ -163,10 +163,8 @@ class ViewController: UIViewController {
     // =====================================================
     // 問題の状況をリセットする関数
     func reset() {
-        // viewを見えるようにする
-        for question in questions {
-            question.isHidden = false
-        }
+        // 問題を初めからにする
+        questionTextView.text = questions[0]
         // ボタンを見えるようにする
         for button in answersStackView.arrangedSubviews {
             button.isHidden = false
